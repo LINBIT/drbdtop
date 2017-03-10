98a4af31ef6e205b5f0a8239bbcf7ce8192630e8
Hayley Swimelar <hayley@linbit.com>
2017-03-09 16:36:55 -0800

    implement multisorting

--- pkg/update/update.go
+++ pkg/update/update.go
@@ -1,19 +1,15 @@
 package update
 
 import (
+	"sort"
+
 	"drbdtop.io/drbdtop/pkg/resource"
 )
-
-// ByResSorter sorts a []*ByRes in place.
-type ByResSorter interface {
-	ByResSort([]*ByRes)
-}
 
 // ResourceCollection is a collection of stats collected organized under their respective resource names.
 type ResourceCollection struct {
 	Map    map[string]*ByRes
-	List   []*ByRes
-	Sorter ByResSorter
+	Sorted *MultiSorter
 }
 
 // Update a collection of ByRes from an Event.
@@ -65,4 +61,68 @@ func (b *ByRes) Update(evt resource.Event) {
 			b.PeerDevices[conn].Update(evt)
 		}
 	}
+}
+
+// https://golang.org/pkg/sort/#example__sortMultiKeys
+type lessFunc func(p1, p2 *ByRes) bool
+
+// MultiSorter implements the Sort interface, sorting the changes within.
+type MultiSorter struct {
+	Resources []*ByRes
+	less      []lessFunc
+}
+
+// Sort sorts the argument slice according to the less functions passed to OrderedBy.
+func (ms *MultiSorter) Sort(changes []*ByRes) {
+	ms.Resources = changes
+	sort.Sort(ms)
+}
+
+// OrderedBy returns a Sorter that sorts using the less functions, in order.
+// Call its Sort method to sort the data.
+func OrderedBy(less ...lessFunc) *MultiSorter {
+	return &MultiSorter{
+		less: less,
+	}
+}
+
+// Len is part of sort.Interface.
+func (ms *MultiSorter) Len() int {
+	return len(ms.Resources)
+}
+
+// Swap is part of sort.Interface.
+func (ms *MultiSorter) Swap(i, j int) {
+	ms.Resources[i], ms.Resources[j] = ms.Resources[j], ms.Resources[i]
+}
+
+// Less is part of sort.Interface. It is implemented by looping along the
+// less functions until it finds a comparison that is either Less or
+// !Less. Note that it can call the less functions twice per call. We
+// could change the functions to return -1, 0, 1 and reduce the
+// number of calls for greater efficiency: an exercise for the reader.
+func (ms *MultiSorter) Less(i, j int) bool {
+	p, q := ms.Resources[i], ms.Resources[j]
+	// Try all but the last comparison.
+	var k int
+	for k = 0; k < len(ms.less)-1; k++ {
+		less := ms.less[k]
+		switch {
+		case less(p, q):
+			// p < q, so we have a decision.
+			return true
+		case less(q, p):
+			// p > q, so we have a decision.
+			return false
+		}
+		// p == q; try the next comparison.
+	}
+	// All comparisons to here said "equal", so just return whatever
+	// the final comparison reports.
+	return ms.less[k](p, q)
+}
+
+// Names sorts resource names by alpha.
+func Names(r1, r2 *ByRes) bool {
+	return r1.Res.Name < r2.Res.Name
 }

c325b62f9c33ec8ffe20c63bda6304bf4ba90336
Hayley Swimelar <hayley@linbit.com>
2017-03-31 12:55:25 -0700

    fix spelling in interval help output

--- drbdtop/main.go
+++ drbdtop/main.go
@@ -35,7 +35,7 @@ var Version string
 func main() {
 	file := flag.String("file", "", "Path to a `file` containing output gathered from polling 'drbdsetup events2 --timestamps --statistics --now'")
 	interval := flag.String("interval", "500ms",
-		"Time to wait between updating drbd status. Valid `time` units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'. Defualt: 500ms")
+		"Time to wait between updating DRBD status. Valid `time` units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'. Default: 500ms")
 	printVersion := flag.Bool("version", false, "Print Version and exit")
 
 	flag.Parse()

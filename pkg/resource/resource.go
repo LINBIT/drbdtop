d8d436bc1a426d279dfce69f6647b02cd8a627eb
Hayley Swimelar <hayley@linbit.com>
2017-07-17 16:48:33 -0700

    unconfigured resources now have 'down' as their role

--- pkg/resource/resource.go
+++ pkg/resource/resource.go
@@ -332,7 +332,11 @@ func NewUnconfiguredRes(name string) Event {
 		TimeStamp: time.Now(),
 		EventType: "exists",
 		Target:    "resource",
-		Fields:    map[string]string{ResKeys.Unconfigured: "true", ResKeys.Name: name},
+		Fields: map[string]string{
+			ResKeys.Unconfigured: "true",
+			ResKeys.Name:         name,
+			ResKeys.Role:         "down",
+		},
 	}
 }
 
